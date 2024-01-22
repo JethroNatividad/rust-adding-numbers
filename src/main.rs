f8e7a3fa76a5cd84359329e2b31900cb51836404
JethroNatividad <natividad.jet@gmail.com>
2024-01-23 06:34:13 +0800

    write program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// Program tha prompts how many to add, then prompts n amount of that times and add all numbers.
+// Inputs: count, n_number
+// Process: add all
+// Outputs: The total is n
+
 fn main() {
     println!("Hello, world!");
 }
