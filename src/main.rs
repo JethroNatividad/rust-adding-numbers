f1110a68ed391ea217d369a28691d0a926d7b98b
JethroNatividad <natividad.jet@gmail.com>
2024-01-23 06:37:04 +0800

    write tests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@
 // Process: add all
 // Outputs: The total is n
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_add_numbers() {
+        assert_eq!(add_numbers(vec![1, 2, 3]), 6);
+        assert_eq!(add_numbers(vec![10, 10, 0, 10]), 30);
+        assert_eq!(add_numbers(vec![99999, 0, 1]), 100000);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
